22fb42ff3e8737489287a8bc9ef0e0bf037b7404
Mateus Matos <mateusmatos2004@gmail.com>
2023-04-20 14:40:58 -0300

    Create objeto.js

---
+++ javascript-dev-aprender/objeto.js
@@ -0,0 +1,16 @@
+let nome = 'Rafael';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'de Sousa',
+    idade: 25,
+    estaAprovado: true,
+    media: 8.5,
+    anoDeInicio: 2019,
+    anoDeConclusao: 2022
+};
+
+console.log(pessoa)
