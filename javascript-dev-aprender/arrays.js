c78539680d7ed50b1cac78689da44d1fdc8b8c8f
Mateus Matos <mateusmatos2004@gmail.com>
2023-04-20 14:46:47 -0300

    Create arrays.js

---
+++ javascript-dev-aprender/arrays.js
@@ -0,0 +1,8 @@
+//Arrays
+
+let familia = [true,26,45,50,17];
+console.log(familia.length)
+console.log(familia[0]);
+
+let dadosColega = ['Daniel', 19, 'Tianguá'];
+console.log(`O nome dele é ${dadosColega[0]}, tem ${dadosColega[1]} anos, e mora na cidade de ${dadosColega[2]}`);
