f021778f29149ca3c34c9f4dbf22ed7389c86782
Mateus Matos <mateusmatos2004@gmail.com>
2023-04-26 14:45:21 -0300

    código mais clean

--- javascript-dev-aprender/mini-projeto8/script.js
+++ javascript-dev-aprender/mini-projeto8/script.js
@@ -12,20 +12,19 @@ const array = [70,70,80];
 console.log(mediaDoAluno(array));
 
 function mediaDoAluno(notas){
-    let media = 0;
+    const media = calcularMedia(notas);
+
+    if(media<60) return 'F';
+    if(media<=69) return 'D';
+    if(media<=79) return 'C';
+    if(media<=89) return 'B';
+    return 'A';
+}
+
+function calcularMedia(array){
     let soma = 0;
-    for(let i = 0; i<notas.length; i++){
-        soma += notas[i];
+    for(let i = 0; i<array.length; i++){
+        soma += array[i];
     }
-    media = soma/notas.length;
-    if(media<60)
-        return 'F';
-    if(media<=60 && media<=69)
-        return 'D';
-    if(media>=70 && media<=79)
-        return 'C';
-    if(media>=80 && media<=89)
-        return 'B';
-    if(media>=90)
-        return 'A';
+    return soma/array.length;
 }
