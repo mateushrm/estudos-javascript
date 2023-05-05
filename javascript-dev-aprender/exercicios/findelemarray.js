1c41a2f9be453918e923da33d64344b2c0db18ca
Mateus Matos <mateusmatos2004@gmail.com>
2023-05-05 14:51:30 -0300

    Create findelemarray.js

---
+++ javascript-dev-aprender/exercicios/findelemarray.js
@@ -0,0 +1,8 @@
+const numeros= [1,4,2,3,4];
+console.log(numeros.indexOf(2));
+console.log(numeros.lastIndexOf(4));
+
+//procurar se existe um elemento na array
+console.log(numeros.indexOf(2)!== -1);
+//ou
+console.log(numeros.includes(2));
