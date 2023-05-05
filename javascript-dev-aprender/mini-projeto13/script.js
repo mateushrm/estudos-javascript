139ff5ec68518fd45b892f35903929d7bf1a135d
Mateus Matos <mateusmatos2004@gmail.com>
2023-05-05 14:14:25 -0300

    mini-projeto 13

---
+++ javascript-dev-aprender/mini-projeto13/script.js
@@ -0,0 +1,15 @@
+//(Mini-projeto 13) - Postagem de BLog
+
+let postagem={
+    titulo:'a',
+    mensagem:'b',
+    autor:'c',
+    visualizacoes:0,
+    comentarios : [
+       {autor:'a', mensagem:'b'},
+       {autor:'a', mensagem:'b'}
+    ],
+    estaAoVivo:true
+}
+
+console.log(postagem);
