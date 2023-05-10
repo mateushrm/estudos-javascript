14c7648df296711fb49e93d6a712bbbeb2a7bd52
Mateus Matos <mateusmatos2004@gmail.com>
2023-05-10 15:29:17 -0300

    Create rem_all.js

---
+++ javascript-dev-aprender/exercicios/rem_all.js
@@ -0,0 +1,15 @@
+let alfabeto = ['a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+'m','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w','x','y','z'];
+console.log(alfabeto);
+
+//Solução 1
+//alfabeto = [];
+//console.log(alfabeto);
+
+//Solução 2
+//alfabeto.length = 0;
+//console.log(alfabeto);
+
+//Solução 3
+alfabeto.splice(0,alfabeto.length);
+console.log(alfabeto);
