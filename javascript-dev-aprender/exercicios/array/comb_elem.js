60ca734b358dee57127418ae002cbd0518ed7d36
Mateus Matos <mateusmatos2004@gmail.com>
2023-05-12 15:07:45 -0300

    Create comb_elem.js

---
+++ javascript-dev-aprender/exercicios/array/comb_elem.js
@@ -0,0 +1,13 @@
+//exercício combinando elementos de um array
+//join
+const vogais = ['a','e','i','o','u'];
+const combinando = vogais.join(',');
+console.log(combinando);
+
+//split
+const pendente = 'not turned in';
+const resultado = pendente.split(' ');
+console.log(resultado);
+
+//join
+console.log(resultado.join('-'));
