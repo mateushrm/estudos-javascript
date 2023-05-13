c5b0fdfce7275928ea734be71b543b657d3f25e3
Mateus Matos <mateusmatos2004@gmail.com>
2023-05-13 14:14:05 -0300

    Create input.js

---
+++ javascript-dev-aprender/exercicios/input.js
@@ -0,0 +1,8 @@
+//praticando um pouco de input em javascript
+let idade = prompt('Olá, qual sua idade?');
+
+if(idade<18){
+    alert('Você é menor de idade!');
+} else {
+    alert('Você é maior de idade!');
+}
